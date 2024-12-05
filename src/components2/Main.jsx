a9610b681eca6287f63445328d73d9ecfeea6120
Yazan <abo.ayash.yazan@gmail.com>
2024-12-05 10:22:08 +0100

    fixed social

--- src/components2/Main.jsx
+++ src/components2/Main.jsx
@@ -8,7 +8,7 @@ function Main({ children }) {
       {/* For Large Screens Only */}
       <div className="hidden lg:flex flex-col h-full relative">
         {/* Social Media Sidebar */}
-        <div className="absolute right-0 -translate-y-72 h-full flex flex-col items-center justify-center space-y-6 bg-opacity-70 z-50">
+        <div className="absolute right-0 top-32 flex flex-col items-center justify-center space-y-6 bg-opacity-70 z-50">
           <a href="#" className="text-white hover:text-yellow-400 px-2 transition-all flex flex-col items-center">
             <FontAwesomeIcon icon={faFacebook} size="lg" />
           </a>
